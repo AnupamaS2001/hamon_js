7fa49db34ab6ae2aea35f94fdb8cf4b09f091fe0
Anupama Suresh <anupamasuresh812@gmail.com>
2024-06-24 17:28:35 +0530

    second largest

---
+++ sec_largest.js
@@ -0,0 +1,30 @@
+var arr=[1,5,9,7,3,9,8,4,6,6,2,10,11,12,10]
+var array=arr.sort((a,b)=>b-a)
+
+console.log(array[1])
+
+
+// var sortedArray=arr.sort()
+// var reversed=sortedArray.reverse()
+// console.log(reversed)
+// var duplicate=[]
+// var set=[]
+// for(let i=0;i<reversed.length;i++){
+//     if(reversed[i]==reversed[i+1]){
+//         duplicate.push(reversed[i])
+//     }
+//     else{
+//         set.push(reversed[i])
+//     }
+// }
+// console.log(duplicate)
+// console.log(set)
+
+// var sortedArray=arr.sort()
+// var len=sortedArray.length
+// var array=[]
+// for(let i=len-1;i>=0;i--){
+//     array.push(sortedArray[i])
+// }
+
+// console.log(array[1])
